d8bcadcac2ebdc96aa9273f240370777e8db78e1
VictorC78 <victorcarlos0000000000@gmail.com>
2023-08-28 16:24:59 -0300

    .

--- marca.php
+++ marca.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Marcas de Qualidade</title>
+    <title></title>
     <meta charset="utf-8">
     <meta name="author" content="templatemo">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     
+    <title>Marcas de Qualidade</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
